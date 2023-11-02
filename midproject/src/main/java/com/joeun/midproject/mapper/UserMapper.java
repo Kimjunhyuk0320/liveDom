9ad9806da3753af6f79c0c46f89d78bbf68eb083
kyocha_nnn <koogc0724@gmail.com>
2023-11-02 10:56:00 +0900

    11/2 10:55

--- midproject/src/main/java/com/joeun/midproject/mapper/UserMapper.java
+++ midproject/src/main/java/com/joeun/midproject/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.joeun.midproject.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.joeun.midproject.dto.UserAuth;
 import com.joeun.midproject.dto.Users;
 
 @Mapper
@@ -20,10 +19,9 @@ public interface UserMapper {
     // 회원 조회
     public Users selectById(String userId) throws Exception;
     
+    
     // 사용자 인증(로그인) - id
     public Users login(String username);
  
-    // 회원 권한 등록
-    public int insertAuth(UserAuth userAuth) throws Exception;
 
 }
